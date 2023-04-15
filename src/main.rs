0661e362c1e0ea4aaaac5af2306cfda306243007
Casey Ransom <cransom@hubns.net>
2023-04-15 13:00:20 -0400

    loops. dim begin

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use image::AnimationDecoder;
 
 use std::{thread, time};
 use std::fs::File;
-use std::env;
 use std::net::UdpSocket;
 
 use clap::Parser;
@@ -14,54 +13,60 @@ use clap::Parser;
 struct Args {
     /// Host
     #[arg(short,long)]
-    host: String
+    target: String,
+
+    #[arg(short,long, default_value_t = 0)]
+     loops: i32,
+
+    #[arg(short,long, default_value_t = 2)]
+    remain: u8,
 
 
+
+    #[arg(short,long)]
+    file: String,
+
+    #[arg(short,long, default_value = "1x1")]
+    dimension: String
 }
 fn main() {
 
     // cli args
+    let args = Args::parse();
 
+    println!("{:?}, {:?}", args.target, args.file);
 
-    let args: Vec<String> = env ::args().collect();
-
-    let host = &args[1];
-
-    let file = &args[2];
-
-    println!("{}, {}", host, file);
-
-    //
-    // udp setup
-
-    //let socket = UdpSocket::send_to
-    //
     // file setup
-    let img = File::open(file).unwrap();
+    // check for file
+    let img = File::open(args.file).unwrap();
 
     let decoder = GifDecoder::new(img).unwrap();
 
-    let mut total_time: u128 = 0;
-    let mut loop_count: u32 = 0;
-    let mut dir: bool = true;
+
+    let mut loop_count: i32 = args.loops;
 
     //
     // filters?
     //
+    let frames = decoder.into_frames().collect_frames().expect("error decoding");
 
-    // send
-    for frame in decoder.into_frames().collect_frames().expect("error decoding gif") {
-        let (width, height) = frame.buffer().dimensions();
-        let (numer, denom) = frame.delay().numer_denom_ms();
-        println!("{}x{}, delay of {}", width, height, numer / denom);
-        println!("{}", frame.buffer().pixels().count());
-        let mut pixel_sequence: Vec<u8> = vec![];
+    loop {
+        for frame in frames.clone() {
+            let (width, height) = frame.buffer().dimensions();
+            let (numer, denom) = frame.delay().numer_denom_ms();
+            let mut pixel_sequence: Vec<u8> = vec![];
 
-        for pixel  in frame.buffer().pixels() {
-            pixel_sequence.extend_from_slice( &[ pixel[0], pixel[1], pixel[2] ]);
-        }
-        send_drgb(&mut pixel_sequence, host);
-        thread::sleep(time::Duration::from_millis(numer as u64 / denom as u64 ));
+            for pixel in frame.buffer().pixels() {
+                pixel_sequence.extend_from_slice( &[ pixel[0], pixel[1], pixel[2] ]);
+            }
+            send_wled(&mut pixel_sequence, &args.target);
+            thread::sleep(time::Duration::from_millis(numer as u64 / denom as u64 ));
+    }
+        match loop_count {
+            0 => continue,
+            1 => break,
+            _ => loop_count -= 1
+        };
 
 
     }
@@ -69,13 +74,16 @@ fn main() {
 
 }
 
-fn send_drgb(seq: &mut Vec<u8>, host: &String) {
+fn send_wled(seq: &mut Vec<u8>, host: &String) {
+
+    // if < 490, drgb = 2. if more, dnrgb
     //drgb
     seq.insert(0,2);
     // timeout
     seq.insert(1,2);
 
     let socket = UdpSocket::bind("0.0.0.0:0").expect("couldn't bind a socket");
+    socket.set_broadcast(true).expect("couldn't set flag to allow broadcasts");
     socket.send_to(&seq, host).expect("failed to send");
 
 
